a111c6f3de8a817c77e82fd3895eadc3dd0e4e28
calebanthony <caleb.m.anthony@gmail.com>
2017-08-20 13:59:25 -0600

    Fix tooltips breaking on hero selection page

--- resources/assets/js/tooltips.js
+++ resources/assets/js/tooltips.js
@@ -3,12 +3,13 @@ import Tooltip from 'tooltip.js';
 /**
  * Initialize the tooltips
  */
-var tooltips = document.querySelectorAll('[tooltip-title]');
-for (var i = 0; i < tooltips.length; i++) {
-    new Tooltip(tooltips[i], {
-        placement: 'bottom',
-        boundariesElement: 'window',
-        title: tooltips[i].getAttribute('tooltip-title'),
-        html: true,
-    })
-}
+document.addEventListener('DOMContentLoaded', function() {
+    var tooltips = document.querySelectorAll('[tooltip-title]');
+    for (var i = 0; i < tooltips.length; i++) {
+        new Tooltip(tooltips[i], {
+            placement: 'bottom',
+            title: tooltips[i].getAttribute('tooltip-title'),
+            html: true,
+        })
+    }
+})
