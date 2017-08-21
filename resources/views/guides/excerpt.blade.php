0b190b7057def676d6fa7e73e01216aebf296fad
calebanthony <caleb.m.anthony@gmail.com>
2017-08-21 08:23:43 -0600

    Clean up guide excerpt on home page

--- resources/views/guides/excerpt.blade.php
+++ resources/views/guides/excerpt.blade.php
@@ -1,10 +1,10 @@
-<div class="columns is-mobile is-multiline is-featured-guide">
+<div class="columns is-mobile is-multiline is-featured-guide is-vertically-aligned">
     <div class="column is-2-desktop is-6-touch is-offset-3-touch">
         <img src="/images/{{ preg_replace('/\s+/', '', strtolower($guide->hero)) }}/profile.png">
     </div>
     <div class="column is-10-desktop is-12-touch">
-        <div class="columns is-mobile">
-            <div class="column is-10-touch is-9-desktop">
+        <div class="columns is-mobile is-multiline">
+            <div class="column is-12-touch is-8-desktop">
                 <a href="/guides/{{ str_replace(' ', '_', $guide->hero) }}/{{ $guide->id }}">
                     <h3 class="title is-5">{{ $guide->name }}</h3>
                 </a>
@@ -12,13 +12,17 @@
                     by {{ $guide->author }} updated {{ date("F j, Y", strtotime($guide->updated_at)) }}
                 </h6>
             </div>
-            <div class="column is-2-touch is-3-desktop has-text-grey-light">
-                <div class="columns is-multiline">
-                    <div class="column is-12 has-text-right is-guide-stat">
+            <div class="column is-12-touch is-4-desktop has-text-grey-light">
+                <div class="columns is-mobile">
+                    <div class="column is-4 has-text-centered is-guide-stat">
+                        {{-- {{ $guide->views }}
+                        <span class="fa fa-comment"></span> --}}
+                    </div>
+                    <div class="column is-4 has-text-centered is-guide-stat">
                         {{ $guide->votes->count() }}
                         <span class="fa fa-thumbs-up"></span>
                     </div>
-                    <div class="column is-12 has-text-right is-guide-stat">
+                    <div class="column is-4 has-text-centered is-guide-stat">
                         {{ $guide->views }}
                         <span class="fa fa-eye"></span>
                     </div>
